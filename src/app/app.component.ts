f8177cf1a57017a7403e660081606b210a6f51ea
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-03-29 10:46:37 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'Manuel'
+
+  mostrarNombre() {
+    console.log(this.nombre)
+  }
 }
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    CardModule,
+    ButtonModule,
+    MenubarModule,
+
+
+
+
+  ],
+
+  imports: [
+    CommonModule,
+
+
+  ]
+})
+export class PrimeNgModule { }
